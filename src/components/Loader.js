f654eca9994f3619ba276f212c69bf046af362b9
Ayush Bhowate <ayushpay1919@gmail.com>
2025-05-27 15:54:50 +0530

    Refactor exercise components: streamline BodyPart and ExerciseCard components, enhance Loader implementation, and improve pagination logic in Exercises for better performance and readability.

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from 'react';
 import { Stack } from '@mui/material';
-import {InfinitySpin} from 'react-loader-spinner' ; 
-const Loader = () => {
-  return (
-     <Stack direction="row" justifyContent="center" alignItems="center" width="100%">
-      <InfinitySpin width='200' color="gray" />   
-     </Stack>
-  )
-}
+import { InfinitySpin } from 'react-loader-spinner';
 
-export default Loader
+const Loader = () => (
+  <Stack direction="row" justifyContent="center" alignItems="center" width="100%">
+    <InfinitySpin color="grey" />
+  </Stack>
+);
+
+export default Loader;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,35 +1,21 @@
-import React , {useState} from 'react' ; 
-import {Box} from '@mui/material' ;
-import { ExitToApp } from '@mui/icons-material';
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
 
-import HeroBanner from '../components/herobanner' ;
-import SearchExercises from '../components/searchExercises' ;
-import Exercises from '../components/exercises' ;
-
+import Exercises from '../components/exercises';
+import SearchExercises from '../components/searchExercises';
+import HeroBanner from '../components/herobanner';
 
 const Home = () => {
-  const [exercises , setExercises] = useState([]);
-  const [bodyPart , setBodyPart] = useState('all') ;
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
 
-  console.log(bodyPart);
-  
   return (
-    <div>
-      <Box>
-        <HeroBanner/>
-        <SearchExercises 
-        setExercises={setExercises}
-         bodyPart={bodyPart} 
-         setBodyPart={setBodyPart}/>
-        <Exercises 
-        exercises={exercises}
-        setExercises={setExercises}
-        bodyPart={bodyPart}
-         />
+    <Box>
+      <HeroBanner />
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
+    </Box>
+  );
+};
 
-      </Box>
-    </div>
-  )
-}
-
-export default Home
+export default Home;
